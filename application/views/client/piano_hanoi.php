f28221490add88842748c86fae1a8a90b82eb13a
Vuong <ndwuong2@gmail.com>
2017-09-05 18:40:24 +0700

    Minor changes to client

--- application/views/client/piano_hanoi.php
+++ application/views/client/piano_hanoi.php
@@ -24,7 +24,7 @@
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700|Monteserrat:500,700" rel="stylesheet">
 
 	<!-- Font Awesome CDN -->
-	<link rel="stylesheet" type="text/css" href="<?php echo fontawesome() ?>">
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 
 	<!-- Bootstrap CSS -->
 	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
